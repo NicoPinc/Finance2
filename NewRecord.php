417ef778ba63379a1184d2fc0827c2b0e0f23210
NikolaPincova <nikoleta.pincova@seznam.cz>
2021-03-29 01:42:52 +0200

    Edited sql table

--- NewRecord.php
+++ NewRecord.php
@@ -9,9 +9,9 @@ if ( ! empty( $_POST['MainFormButton'] ) ) { /* print_r($_POST); exit */
 
     //submit
 if(is_null($cat)){
-    $query = "INSERT INTO allcash2 (cash_Value, cash_Date, cash_Cat, cash_Desc, type_Id, usr_Id) VALUES ('$amount','$date',NULL, '$descr', '$type', '$userName')";
+    $query = "INSERT INTO allcash2 (cash_Value, cash_Date, cat_Id, cash_Desc, type_Id, usr_Id) VALUES ('$amount','$date','9', '$descr', '$type', '$userName')";
 }else{
-    $query = "INSERT INTO allcash2 (cash_Value, cash_Date, cash_Cat, cash_Desc, type_Id, usr_Id) VALUES ('$amount','$date','$cat', '$descr', '$type', '$userName')";
+    $query = "INSERT INTO allcash2 (cash_Value, cash_Date, cat_Id, cash_Desc, type_Id, usr_Id) VALUES ('$amount','$date','$cat', '$descr', '$type', '$userName')";
 }
 
     //$query = "INSERT INTO allcash2 (cash_Value, cash_Date, cash_Cat, cash_Desc, type_Id, usr_Id) VALUES ('$amount','$date','$cat', '$descr', '$type', '$userName')";
@@ -39,7 +39,7 @@ function autoSubmit()
 <div class="card " style=" width: 33%/*fit-content*/; display: -webkit-inline-box; padding: 25px; min-width: 33%;">
 <form action="" method="POST" name="RecType">
     <label class="checktext" for="type_Expense">
-    <input onclick="autoSubmit();" type="radio" class="radio" id="type_Expense" name="newRECtype" value="Expense"<?php if((isset($_POST['newRECtype']) && $_POST['newRECtype'] == 'Expense')) echo ' checked="checked"'?> checked/>
+    <input onclick="autoSubmit();" type="radio" class="radio" id="type_Expense" name="newRECtype" value="Expense"<?php if(isset($_POST['newRECtype']) && $_POST['newRECtype'] == 'Expense') echo ' checked="checked"'?> checked/>
     <span>Výdej</span></label>
     <label class="checktext" for="type_Earning">
     <input onchange="autoSubmit();" type="radio" class="radio" id="type_Earning" name="newRECtype" value="Earnings" <?php if(isset($_POST['newRECtype']) && $_POST['newRECtype'] == 'Earnings')  echo ' checked="checked"' ?> />
