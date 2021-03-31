dc12c3734c219c7e984b9c52a3be4014976f999b
NikolaPincova <nikoleta.pincova@seznam.cz>
2021-03-31 19:38:49 +0200

    filtrovani dokonceno.. alespon nejak

--- Zaznamy/zalozni.php
+++ Zaznamy/zalozni.php
@@ -1,8 +1,8 @@
-== zaznamy filtruji zatim jenom all users a all money ==
 <?php
 include("./DBconn.php");
 
 $startOfMonth = date("Y-m-01");
+$today = date("Y-m-d");
 $getend = strtotime('Y-m-01');
 $endOfMonth = date("Y-m-01", strtotime("+1 month, $getend"));
 
@@ -20,6 +20,7 @@ $endOfMonth = date("Y-m-01", strtotime("+1 month, $getend"));
             <option value="Bert" > Bert</option>
           </select>
         </div>
+        <!--
         <div class="form-group">
           <select class="form-control" name="type" id="">
             <option value="Všechny Peníze" selected>Všechny Peníze</option>
@@ -27,15 +28,15 @@ $endOfMonth = date("Y-m-01", strtotime("+1 month, $getend"));
             <option value="Vklady">Earnings</option>
             <option value="Úspory">Savings</option>                
           </select>
-        </div>
+        </div> -->
         <div class="form-group">
           <select class="form-control" name="date" id="">
-            <option value="Za celé období" selected>Za celé období</option>
-            <option value="Poslední měsíc">Poslední měsíc</option>              
+            <option value="Poslední měsíc" selected>Za poslední měsíc</option> 
+            <option value="Za celé období" >Za celé období</option>
           </select>
         </div>
         <div class="form-group">
-          <button type="submit" name="button" class="btn btn-dark">Show</button>
+          <button type="submit" name="button" class="btn btn-dark">Filtruj</button>
         </div>
       </div>
     </form>
@@ -47,13 +48,6 @@ $endOfMonth = date("Y-m-01", strtotime("+1 month, $getend"));
       $user = $_POST['user'];
       $type = $_POST['type'];
       $time = $_POST['date'];
-      /**
-      if(isset($_POST['button'])) {
-        echo "<p> Zobrazuji data : ". $user .  " - " . $type . "</p>";
-      }else{
-        echo "<p> ← Vyber záznam</p>";
-      } 
-      */
       ?>
         <div <?php if (!isset($_POST['button'])) echo 'hidden' ?>>
           <div class="row">
@@ -61,7 +55,7 @@ $endOfMonth = date("Y-m-01", strtotime("+1 month, $getend"));
           </div>
           <div class="row">
             <div class="col">
-              <?php echo " " .$user. " - ". $type. " - " .$time;?>
+              <?php echo " " .$user. " - " .$time;?>
             </div>
           </div>
         </div>
@@ -86,63 +80,49 @@ $endOfMonth = date("Y-m-01", strtotime("+1 month, $getend"));
           <thead>
             <tr>
               <th scope="col">Datum</th>
-              <th scope="col">ID</th>
-              <th scope="col" style="padding-left: 10%;">Pozn.</th>
+              <th scope="col" hidden>ID</th>
+              <th scope="col">Pozn.</th>
               <th scope="col">Kategorie</th>
               <th scope="col" <?php if($user != 'Všichni Uživatelé') echo 'hidden' ?> >Kdo</th>
-              <th scope="col" style="padding-left: 3%;" <?php if($type == 'Vklady' || $type == 'Úspory') echo 'hidden' ?>>Typ</th>
+              <th scope="col" style="padding-left: 3%;">Typ</th>
               <th scope="col"style="text-align: end;">Částka</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <?php
+            <tr>   
+              <?php // ORDER BY cash_Date DESC
                 $sql = "SELECT * FROM allcash2 c
                   RIGHT JOIN purchase_type pt ON c.type_Id = pt.type_Id
                   RIGHT JOIN pur_cat cat ON c.cat_Id = cat.cat_Id
-                  INNER JOIN users u ON c.usr_Id = u.usr_Id";
-                if($time == "Poslední měsíc"){
-                  $sql .= " WHERE c.cash_Date BETWEEN '" . $startOfMonth ."' AND '" . $endOfMonth. "'  ORDER BY cash_Date DESC";
-                }elseif(){
-                  
-                }else{
-                $sql .= " ORDER BY cash_Date DESC";
-                }
+                  INNER JOIN users u ON c.usr_Id = u.usr_Id ";
+                  if($user != 'Všichni Uživatelé'){
+                    $sql .= " WHERE u.usr_Name = '". $user."' ";
+                  }
+                  if($time == 'Poslední měsíc' && $user != 'Všichni Uživatelé'){
+                    $sql .= " AND cash_Date BETWEEN '". $startOfMonth."' AND '".$today."' ";
+                  }elseif($time == 'Poslední měsíc'){
+                    $sql .= " WHERE cash_Date BETWEEN '". $startOfMonth."' AND '".$today."' ";
+                  }
+                //var_dump($sql);
+                //$sql .= " ORDER BY cash_Date DESC";
                 $res = $conn->query($sql);
 
                 if($res->num_rows > 0){
                   while ($row= $res->fetch_assoc()){
-                    if($user != 'Všichni Uživatelé' && $type == 'Výdaje'){
+                    if($user != 'Všichni Uživatelé'){
                       echo "<tr>";
                       echo "<td>" . $row['cash_Date'] . "</td>";
-                      echo "<td>" . $row['cash_Id'] . "</td>";
-                      echo "<td style='padding-left: 10%;'>" . $row['cash_Desc'] . "</td>";
+                      echo "<td hidden>" . $row['cash_Id'] . "</td>";
+                      echo "<td>" . $row['cash_Desc'] . "</td>";
                       echo "<td>" . $row['cat_Name'] . "</td>";
                       echo "<td style='padding-left: 3%;' >" . $row['type_Name'] . "</td>";
-                      echo "<td style='text-align: end;'>" . $row['cash_Value'] . "</td>";
-                      echo "</tr>";
-                    }elseif($user != 'Všichni Uživatelé' && $type == 'Všechny Peníze'){
-                      echo "<tr>";
-                      echo "<td>" . $row['cash_Date'] . "</td>";
-                      echo "<td>" . $row['cash_Id'] . "</td>";
-                      echo "<td style='padding-left: 10%;'>" . $row['cash_Desc'] . "</td>";
-                      echo "<td>" . $row['cat_Name'] . "</td>";
-                      echo "<td style='padding-left: 3%;' >" . $row['type_Name'] . "</td>";
-                      echo "<td style='text-align: end;'>" . $row['cash_Value'] . "</td>";
-                      echo "</tr>";
-                    }elseif($user != 'Všichni Uživatelé' && $type == 'Vklady' || $type == 'Úspory'){
-                      echo "<tr>";
-                      echo "<td>" . $row['cash_Date'] . "</td>";
-                      echo "<td>" . $row['cash_Id'] . "</td>";
-                      echo "<td style='padding-left: 10%;'>" . $row['cash_Desc'] . "</td>";
-                      echo "<td>" . $row['cat_Name'] . "</td>";
                       echo "<td style='text-align: end;'>" . $row['cash_Value'] . "</td>";
                       echo "</tr>";
                     }else{
                       echo "<tr>";
                       echo "<td>" . $row['cash_Date'] . "</td>";
-                      echo "<td>" . $row['cash_Id'] . "</td>";
-                      echo "<td style='padding-left: 10%;'>" . $row['cash_Desc'] . "</td>";
+                      echo "<td hidden>" . $row['cash_Id'] . "</td>";
+                      echo "<td>" . $row['cash_Desc'] . "</td>";
                       echo "<td>" . $row['cat_Name'] . "</td>";
                       echo "<td>" . $row['usr_Name'] . "</td>";
                       echo "<td style='padding-left: 3%;' >" . $row['type_Name'] . "</td>";
@@ -164,41 +144,9 @@ $endOfMonth = date("Y-m-01", strtotime("+1 month, $getend"));
   <div <?php if (isset($_POST['button'])) echo 'hidden' ?>>
   <div class="row">
     <div class="card flex-fill w-100">
-      Hello
+
     </div>
   </div>
 </div>
 
 </div>
-
-<script>  
-$(document).ready(function(){  
-    $('#editable_table').Tabledit({
-      url:'action.php',
-      columns:{
-      identifier:[0, "id"],
-      editable: [6, "cash_Value"],
-      },
-      editButton: false,
-      restoreButton: false,
-      buttons: {
-          delete: {
-              class: 'btn btn-sm btn-danger',
-              html: '<span class="glyphicon glyphicon-trash"></span> &nbsp DELETE',
-              action: 'delete'
-          },
-          confirm: {
-              class: 'btn btn-sm btn-default',
-              html: 'Are you sure?'
-          }
-      },
-      onSuccess:function(data, textStatus, jqXHR)
-      {
-      if(data.action == 'delete')
-      {
-        $('#'+data.id).remove();
-      }
-      }
-    });
-});  
-</script>
